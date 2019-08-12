9bf2ac0eb27a1dfc10bfa3e676bff45f2c1cbd72
Mariana Zaloga <marjana985@gmail.com>
2019-08-12 11:25:14 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -1,18 +1,53 @@
 document.querySelector('.timestamp').innerText = (new Date()).toLocaleTimeString();
-document.querySelector('.ajax-html').addEventListener('click', getHtmlAjax);
 
 const XHR_STATE_DONE = 4;
 const HTTP_STATUS_CODE = 200;
 
-function getHtmlAjax() {
+// JSON Sample
+
+document.querySelector('.ajax-json').addEventListener('click', getAjaxJson);
+
+function getAjaxJson() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === XHR_STATE_DONE &&
             xhr.status === HTTP_STATUS_CODE) {
-            document.querySelector('.html-container').innerHTML = xhr.responseText;
+            const clientData = JSON.parse(xhr.responseText);
+            document.querySelector('.client-name').innerText = clientData.name;
+            document.querySelector('.account-balance').innerText = clientData.balance;
         }
     }
-    xhr.open('GET', 'client-data.html', true);
+    xhr.open('GET', 'client-data.json', true);
     xhr.send();
 }
 
+document.querySelector('.fetch-json').addEventListener('click', fetchJson);
+
+function fetchJson() {
+    fetch('client-data.json')
+        .then(response => response.json())
+        .then(clientData => {
+            document.querySelector('.client-name').innerText = clientData.name;
+            document.querySelector('.account-balance').innerText = clientData.balance;
+        });
+}
+
+document.querySelector('.login-form input[type=submit]')
+    .addEventListener('click', login);
+
+function login(e) {
+    e.preventDefault();
+    fetch('login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+            name: document.querySelector('.login-form input[name=name]').value,
+            email: document.querySelector('.login-form input[name=email]').value,
+            mesagge: document.querySelector('.login-form textarea[name=message]').value
+        })
+    })
+        .then(_ => document.querySelector('.login-form').reset());
+}
